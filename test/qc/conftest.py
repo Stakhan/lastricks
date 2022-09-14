29f361ebdbcaa2cbc30819fef3eb75d1c1a7c2e6
Elie-Alban LESCOUT <elie-alban.lescout@ensg.eu>
2022-09-14 10:11:13 +0200

    moved generate LasData to common location for other module's usage

--- test/qc/conftest.py
+++ test/qc/conftest.py
@@ -3,30 +3,7 @@ import pytest
 import numpy as np
 from pathlib import Path
 from numpy.random import random_sample, randint
-
-def generate_LasData(length=10):
-    "Generates a random LAS/LAZ representation."
-
-    allX, allY, allZ = ( random_sample(size=(length,))*100,
-                         random_sample(size=(length,))*100,
-                         random_sample(size=(length,))*100 )
-
-    Xmin, Ymin, Zmin = ( np.floor(np.min(allX)),
-                         np.floor(np.min(allY)),
-                         np.floor(np.min(allZ)) )
-
-    Xmax, Ymax, Zmax = ( np.ceil(np.max(allX)),
-                         np.ceil(np.max(allY)),
-                         np.ceil(np.max(allZ)) )
-
-    mock_hdr = laspy.LasHeader(version="1.4", point_format=6)
-    mock_hdr.offsets, mock_hdr.scales = [0.0,0.0,0.0],[0.001,0.001,0.001]
-    mock_hdr.mins, mock_hdr.maxs = [Xmax,Ymax,Zmax], [Xmax,Ymax,Zmax]
-
-    mock_las = laspy.LasData(mock_hdr)
-    mock_las.X, mock_las.Y, mock_las.Z = allX, allY, allZ
-
-    return mock_las
+from ..lasdata_generator import generate_LasData
 
 @pytest.fixture
 def las1():
@@ -58,7 +35,7 @@ def las4():
 @pytest.fixture
 def las5():
     """The same example as `las4` but after transformations for delivery.
-       A virtual point was added at the end.&
+       A virtual point was added at the end.
     """
     las5 = generate_LasData(length=11)
     las5.classification = [1,2,3,4,5,6,9,17,64,65,66]
