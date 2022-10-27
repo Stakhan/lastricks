8b11c5192f6ac43592fa80484457460e235bedcf
EAL <elie-alban.lescout@eurosense.com>
2022-10-28 00:52:49 +0200

    WIP: refactor compare deliveries

--- use_cases/compare_deliveries/compare_deliveries.py
+++ use_cases/compare_deliveries/compare_deliveries.py
@@ -34,74 +34,172 @@ class Timer:
     def __str__(self):
         return str(datetime.now()-self.start_time)
 
-def generate_vector(las, timer, crs='EPSG:2154'):
-
-    print(f"{timer} | Computing mask for {path.stem}")
-    mask = las.error_mask == 1
-    x, y = las.x[mask], las.y[mask]
-    print(f"{timer} | Extracting points from {path.stem}")
-    points = [ Point(x[i], y[i]) for i in range(sum(mask)) ]
-    ds = gpd.GeoSeries(
-        points,
-        crs=crs
-        )
-    print(f"{timer} | Computing buffer")
-    buffers = ds.buffer(1, resolution=6)
-    print(f"{timer} | Simplifying buffer")
-    buffers_simplified = buffers.unary_union
-    print(f"{timer} | Explode MultiPolygon to Polygons")
-    final_gs = gpd.GeoSeries([
-        buffers_simplified
-    ]).explode(index_parts=True)
-    
-    return final_gs, timer
 
 class DeliveryComparator:
     def __init__(
             self,
             delivery_1_path,
             delivery_2_path,
-            vector_reference_path,
+            vect_ref_path,
             output_path,
             crs = 'EPSG:2154'
         ):
+        self.timer = Timer()
         self.main_input = InputManager(delivery_1_path)
         self.secondary_input = InputManager(delivery_2_path)
+        self.output_path = output_path
+        self.crs = crs
+        output_path.mkdir(exist_ok=True)
+
+        # Loading vector data used as reference (possibly in many layers)
+        layers = fiona.listlayers(vect_ref_path) 
+        refs = [
+            gpd.read_file(vect_ref_path,
+                          layer=layer,
+                          crs=crs).geometry
+            for layer in layers if layer != 'Default'
+            ]
+        self.ref = pd.concat(refs)
+
+        self.stats = {
+            'index': [],
+            'nb_changes': [],
+            'nb_expected_changes': [],
+            'coverage_expected_changes_%': [],
+            'nb_missing_changes': []
+        }
+
+    def generate_vector(self, las):
+        self.log("Computing mask")
+        mask = las.error_mask == 1
+        x, y = las.x[mask], las.y[mask]
+        self.log("Extracting points")
+        points = [ Point(x[i], y[i]) for i in range(sum(mask)) ]
+        ds = gpd.GeoSeries(
+            points,
+            crs=self.crs
+            )
+        self.log("Computing buffer")
+        buffers = ds.buffer(1, resolution=6)
+        self.log("Simplifying buffer")
+        buffers_simplified = buffers.unary_union
+        self.log("Explode MultiPolygon to Polygons")
+        final_gs = gpd.GeoSeries([
+            buffers_simplified
+        ]).explode(index_parts=True)
+    
+        return final_gs
+    
+    def get_local_ref(self, tile_name):
+        try:
+            xmin, ymax = [int(v) for v in tile_name.split('_')]
+        except Exception:
+            raise ValueError(
+                f"{tile_name} is not a valid name to proceed. "
+                "Must be of form '<xmin>_<ymax>'."
+            )
+        xmax = xmin + 1000
+        ymin = ymax - 1000
+        return self.ref.cx[xmin:xmax, ymin:ymax]
+
+    def intersection_mask(self, local_ref, cc_vect):
+        """Returns a mask which is `True` when the
+           reference vector matches with a change.
+
+        Args:
+            local_ref (geopandas.GeoSeries): local references
+            cc_vect (geopandas.GeoSeries): change cloud as vector
+        """
+        inter_mask = []
+        for ref_geom in local_ref:
+            inter_mask.append(
+            any( test.apply(ref_geom.intersects) )
+            )
+        return inter_mask
+    
+    def compute_stats(
+        self,
+        tile_name,
+        cc_vect,
+        ):
+        local_ref = self.get_local_ref(tile_name)
+        nb_inter = sum(
+                self.intersection_mask(local_ref, cc_vect)
+            )
+        self.stats['index'].append( tile_name )
+        self.stats['nb_changes'].append( len(cc_vect) )
+        self.stats['nb_expected_changes'].append( len(local_ref) )
+        self.stats['coverage_expected_changes_%'].append( nb_inter / len(local_ref) * 100 if len(local_ref) > 0 else 100 )
+        self.stats['nb_missing_changes'].append( len(local_ref) - nb_inter )
+
+    def produce_report(self):
+        report_out = self.output_path / 'comparison_report'
+        report_out.mkdir(exist_ok=True)
+
+        # Stats per file
+        stats_df = pd.DataFrame( data=self.stats )
+        stats_df.to_csv(report_out / 'stats_per_file.csv', mode='a')
+
+        # Summary
+        stats_summary = {
+            'index': ['TOTAL'],
+            'nb_changes': [sum(self.stats['nb_changes'])],
+            'nb_expected_changes': [sum(self.stats['nb_expected_changes'])],
+            'coverage_expected_changes_%': [mean(self.stats['coverage_expected_changes_%'])],
+            'nb_missing_changes':[sum(self.stats['nb_missing_changes'])]
+        }
+        stats_sum_df = pd.DataFrame( data=stats_summary )
+        stats_sum_df.to_csv(report_out / 'stats_summary.csv', mode='a')
+
+    def compare(self):
+        for path in self.main_input:
+            if not (output_path / f'change_map_{path.stem}.shp').exists():
+                try:
+                    self.log(f"Reading {path}")
+                    las = self.main_input.query_las(path)
+                    gt_las = self.secondary_input.query_las(path)
+                    
+                    self.log("Computing change cloud...")
+                    cc_las = change_cloud(las, gt_las)
+                    cc_vect = self.generate_vector(cc_las)
+                    
+                    self.log("Computing stats...")
+                    self.compute_stats(path.stem, cc_vect)
+
+                    self.log("Writing to SHP")
+                    cc_vect.to_file(
+                        output_path / f'change_map_{path.stem}.shp',
+                        crs=self.crs
+                    )
+                    
+                    self.log("Producing report")
+                    self.produce_report()
+
+                    self.log("Done")
+                except Exception as e:
+                    print(f"{e.__class__.__name__} when trying to process {path.name}")
+            else:
+                self.log(f"Skipping {path.stem}...")
+
+    def log(self, msg):
+        print(str(self.timer)+" | "+msg)
 
 if __name__ == '__main__':
-    timer = Timer()
+    
 
     root = Path(__file__).resolve().parents[2] / 'tmp' / 'compare_deliveries'
     
-    main_input_path = root / 'delivery_1' / 'laz' / '921000_6541000.laz'
-    secondary_input_path = root / 'delivery_2' / 'laz' / '921000_6541000.laz'
-    vector_reference_path = root / 'delivery_1' / 'report' / 'FR_038_3_Block_PK_AREA1_Naskatech_20220922.gpkg'
+    delivery_1_path = root / 'delivery_1' / 'laz'
+    delivery_2_path = root / 'delivery_2' / 'laz'
+    vect_ref_path = root / 'delivery_1' / 'report' / 'FR_038_3_Block_PK_AREA1_Naskatech_20220922.gpkg'
     output_path = root / 'output'
     crs = 'EPSG:2154'
     
-    output_path.mkdir(exist_ok=True)
-
-    main_input = InputManager(main_input_path)
-    secondary_input = InputManager(secondary_input_path)
-    
-    
-
-    for path in main_input:
-        print(f"{timer} | Reading {path}")
-        las = main_input.query_las(path)
-        gt_las = secondary_input.query_las(path)
-        print(f"{timer} | Computing change cloud...")
-        cc_las = change_cloud(las, gt_las)
-        vector_data, timer = generate_vector(cc_las, timer)
-        print(f"{timer} | Writing to SHP")
-        vector_data.to_file(
-            output_path / f'change_map_{path.stem}.shp',
-            crs=crs
-        )
-        print(f"{timer} | Done")
-    
-    ## TODO
-    ## Generate statistics on the fly
-    # For each file (1km*1km tile):
-    # + the number of changes in that file
-    # + percentage of highlighted errors intersecting with changes
+    delivery_comparator = DeliveryComparator(
+        delivery_1_path,
+        delivery_2_path,
+        vect_ref_path,
+        output_path,
+        crs = crs
+    )
+    delivery_comparator.compare()
