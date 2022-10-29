227ab660032a2166b3b5f027570919fec6aec639
Elie-Alban LESCOUT <elie-alban.lescout@ensg.eu>
2022-10-29 11:43:08 +0200

    adding error type field and producing missing changes vector layer

--- use_cases/compare_deliveries/compare_deliveries.py
+++ use_cases/compare_deliveries/compare_deliveries.py
@@ -51,15 +51,16 @@ class DeliveryComparator:
         self.output_path = output_path
         self.crs = crs
         output_path.mkdir(exist_ok=True)
+        (output_path / 'missing_changes').mkdir(exist_ok=True)
 
         # Loading vector data used as reference (possibly in many layers)
         layers = fiona.listlayers(vect_ref_path) 
-        refs = [
-            gpd.read_file(vect_ref_path,
-                          layer=layer,
-                          crs=crs).geometry
-            for layer in layers if layer != 'Default'
-            ]
+        refs = []
+        for layer in layers:
+            if layer != 'Default':
+                ref_df = gpd.read_file(vect_ref_path, layer=layer, crs='EPSG:2154')
+                ref_df['error_type'] = layer
+                refs.append( ref_df )
         self.ref = pd.concat(refs)
 
         self.stats = {
@@ -89,7 +90,9 @@ class DeliveryComparator:
             buffers_simplified
         ]).explode(index_parts=True)
     
-        return final_gs
+        final_gdf = gpd.GeoDataFrame(geometry=final_gs)
+        final_gdf['error_type'] = 'Unknown'
+        return final_gdf
     
     def get_local_ref(self, tile_name):
         try:
@@ -103,35 +106,68 @@ class DeliveryComparator:
         ymin = ymax - 1000
         return self.ref.cx[xmin:xmax, ymin:ymax]
 
-    def intersection_mask(self, local_ref, cc_vect):
-        """Returns a mask which is `True` when the
-           reference vector matches with a change.
+    def intersection_masks(self, local_ref, cc_vect):
+        """Returns a list of interesection masks between
+           each reference geometry and all the changes.
 
         Args:
             local_ref (geopandas.GeoSeries): local references
-            cc_vect (geopandas.GeoSeries): change cloud as vector
+            cc_vect (geopandas.GeoSeries): change vector map (geometries only)
         """
-        inter_mask = []
+        inter_masks = []
         for ref_geom in local_ref:
-            inter_mask.append(
-            any( cc_vect.apply(ref_geom.intersects) )
+            inter_masks.append(
+                cc_vect.apply(ref_geom.intersects)
             )
-        return inter_mask
+        return inter_masks
     
+
+            
+
     def compute_stats(
         self,
         tile_name,
         cc_vect,
         ):
+        """Compute intersection statistics
+
+        Args:
+            tile_name (str): name of the tile for which the statistics
+                are computed
+            cc_vect (geopandas.GeoDataFrame): change vector map
+        """
         local_ref = self.get_local_ref(tile_name)
+        error_types = local_ref['error_type'].unique().tolist()
+        missing_changes = []
+        all_i_masks = []
+        local_inter = []
+        for et in error_types:
+            et_mask = local_ref['error_type'] == et
+            # Extracting intersection masks
+            i_masks = self.intersection_masks(
+                local_ref.geometry[et_mask],
+                cc_vect.geometry
+                )
+            # Recording missing change
+            missing_mask = [not any(mask) for mask in i_masks]
+            missing_changes.append( local_ref[et_mask][missing_mask] )
+            # Recording error type
+            cc_vect['error_type'][sum(i_masks).astype(bool)] = et
+            local_inter.append(np.sum(i_masks)>0)
+        
         nb_inter = sum(
-                self.intersection_mask(local_ref, cc_vect)
+                local_inter
             )
         self.stats['index'].append( tile_name )
         self.stats['nb_changes'].append( len(cc_vect) )
         self.stats['nb_expected_changes'].append( len(local_ref) )
         self.stats['coverage_expected_changes_%'].append( nb_inter / len(local_ref) * 100 if len(local_ref) > 0 else 100 )
         self.stats['nb_missing_changes'].append( len(local_ref) - nb_inter )
+
+        if len(missing_changes) > 0:
+            return cc_vect, pd.concat(missing_changes)
+        else:
+            return cc_vect, missing_changes
 
     def produce_report(self):
         report_out = self.output_path / 'comparison_report'
@@ -165,20 +201,25 @@ class DeliveryComparator:
                     cc_vect = self.generate_vector(cc_las)
                     
                     self.log("Computing stats...")
-                    self.compute_stats(path.stem, cc_vect)
+                    cc_vect, missing_changes = self.compute_stats(path.stem, cc_vect)
 
                     self.log("Writing to SHP")
                     cc_vect.to_file(
                         output_path / f'change_map_{path.stem}.shp',
                         crs=self.crs
                     )
+                    if len(missing_changes) > 0:
+                        missing_changes.to_file(
+                            output_path / 'missing_changes' / f'missing_changes_{path.stem}.shp',
+                            crs=self.crs
+                        )
                     
                     self.log("Producing report")
                     self.produce_report()
 
                     self.log("Done")
                 except Exception as e:
-                    if True:
+                    if False:
                         print(f"{e.__class__.__name__} when trying to process {path.name}")
                         continue
                     else:
@@ -196,6 +237,11 @@ if __name__ == '__main__':
     delivery_1_path = root / '03_Back2020909'
     delivery_2_path = root / '04_Back20220928' / 'laz'
     vect_ref_path = root / '03_Back2020909' / 'raport' / 'FR_038_3_Block_PK_AREA1_Naskatech_20220922.gpkg'
+    # root = Path(__file__).resolve().parents[2] / 'tmp' / 'compare_deliveries'
+    # delivery_1_path = root / 'delivery_1' / 'laz'
+    # delivery_2_path = root / 'delivery_2' / 'laz'
+    # vect_ref_path = root / 'delivery_1' / 'report' / 'FR_038_3_Block_PK_AREA1_Naskatech_20220922.gpkg'
+    
     output_path = Path(__file__).parent / 'output'
     crs = 'EPSG:2154'
 
