4518a5a0f5c4aab564507e8e5bdd9e38968d988c
Elie-Alban LESCOUT <elie-alban.lescout@eurosense.com>
2023-05-30 17:50:01 +0200

    more resilient condition testing

--- lastricks/ops/las.py
+++ lastricks/ops/las.py
@@ -4,8 +4,8 @@ from laspy import LasData
 def concat_las(las1: LasData, las2: LasData):
     hdr1, hdr2 = las1.header, las2.header
     assert hdr1.point_format == hdr2.point_format
-    assert hdr1.scales == hdr2.scales
-    assert hdr1.offsets == hdr2.offsets
+    assert list(hdr1.scales) == list(hdr2.scales)
+    assert list(hdr1.offsets) == list(hdr2.offsets)
     lasconcat = LasData(las1.header)
     for dim in lasconcat.header.point_format.dimension_names:
         lasconcat[dim] = np.concatenate((las1[dim], las2[dim]))
